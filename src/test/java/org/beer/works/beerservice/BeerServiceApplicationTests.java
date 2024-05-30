97f2f36c8099c7ecfcecb85e4fcf24a3b3739677
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2024-05-29 18:26:53 -0600

    First Commit

---
+++ src/test/java/org/beer/works/beerservice/BeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.beer.works.beerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
