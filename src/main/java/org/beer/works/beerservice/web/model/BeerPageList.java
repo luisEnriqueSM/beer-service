600e2244a306e880803ad4acfe0bc71e37d75e0c
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2024-05-30 10:14:27 -0600

    Adding Web Data model. Closes #2

---
+++ src/main/java/org/beer/works/beerservice/web/model/BeerPageList.java
@@ -0,0 +1,17 @@
+package org.beer.works.beerservice.web.model;
+
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public class BeerPageList extends PageImpl<BeerDto> {
+
+    public BeerPageList(List<BeerDto> content, Pageable pageable, long total) {
+        super(content, pageable, total);
+    }
+
+    public BeerPageList(List<BeerDto> content) {
+        super(content);
+    }
+}
---
+++ src/main/java/org/beer/works/beerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,5 @@
+package org.beer.works.beerservice.web.model;
+
+public enum BeerStyleEnum {
+    LAGER, PISLSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
