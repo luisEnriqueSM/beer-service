9828f11976450832f7e68d09ece41cc85ef0081b
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2024-07-22 17:06:13 -0600

    Adding Beer repository closes #5

---
+++ src/main/java/org/beer/works/beerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package org.beer.works.beerservice.repositories;
+
+import org.beer.works.beerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
