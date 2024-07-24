de1f0853b5e86573f8c4e7f7b4478eab3550ddd8
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2024-07-24 11:59:05 -0600

    Added ControllerAdivce class and add validations for Controller

--- src/main/java/org/beer/works/beerservice/web/controller/BeerController.java
+++ src/main/java/org/beer/works/beerservice/web/controller/BeerController.java
@@ -3,6 +3,7 @@ package org.beer.works.beerservice.web.controller;
 import org.beer.works.beerservice.web.model.BeerDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -18,13 +19,13 @@ public class BeerController {
     }
 
     @PostMapping
-    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto){
+    public ResponseEntity saveNewBeer(@RequestBody @Validated BeerDto beerDto){
         // todo impl
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto){
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody @Validated BeerDto beerDto){
         // todo impl
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
