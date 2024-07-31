7be885ad2be917a9f5a97f2d979798e2abf0287d
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2024-07-31 16:55:46 -0600

    Adding Jackson config for BeerDto's dates. closes #15

--- src/main/java/org/beer/works/beerservice/web/model/BeerDto.java
+++ src/main/java/org/beer/works/beerservice/web/model/BeerDto.java
@@ -1,5 +1,6 @@
 package org.beer.works.beerservice.web.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Null;
@@ -26,9 +27,11 @@ public class BeerDto {
     private Integer version;
 
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
 
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime lastModifiedDate;
 
     @NotBlank
@@ -41,6 +44,7 @@ public class BeerDto {
     @NotNull
     private Long upc;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @Positive
     @NotNull
     private BigDecimal price;
