dd3b050776dd164b0315e7dd7459db9c084523b1
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2024-08-05 17:18:10 -0600

    Add spring.jpa property, create data.sql file to initialize data, disable BeerLoader. closes #20

--- target/generated-sources/annotations/org/beer/works/beerservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/org/beer/works/beerservice/web/mappers/BeerMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-08-01T10:10:19-0600",
+    date = "2024-08-05T16:48:50-0600",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.3 (Amazon.com Inc.)"
 )
 @Component
