bcb5729bb3ddddfd81d4fc8ed00e5fbb11be7b86
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2024-08-09 13:37:33 -0600

    Hibernate Errors on Startup with MySQL, set varchar length for ID in Beer entity, fix application-localmysql.properties file. Closes #28

--- src/main/java/org/beer/works/beerservice/domain/Beer.java
+++ src/main/java/org/beer/works/beerservice/domain/Beer.java
@@ -21,7 +21,7 @@ public class Beer {
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
-    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID id;
 
     @Version
