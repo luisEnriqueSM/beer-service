ba71e43d6fcd315799926bfb183b1a84dfc23c87
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2024-08-09 23:36:34 -0600

    Enable BeerLoader component to load data if were empty, fix hibernate dialect property

--- src/main/java/org/beer/works/beerservice/bootstrap/BeerLoader.java
+++ src/main/java/org/beer/works/beerservice/bootstrap/BeerLoader.java
@@ -2,20 +2,18 @@ package org.beer.works.beerservice.bootstrap;
 
 import org.beer.works.beerservice.domain.Beer;
 import org.beer.works.beerservice.repositories.BeerRepository;
+import org.beer.works.beerservice.web.model.BeerStyleEnum;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
-import java.util.UUID;
 
-//@Component
+@Component
 public class BeerLoader implements CommandLineRunner {
 
     public static final String BEER_1_UPC = "0631234200036";
     public static final String BEER_2_UPC = "0631234300019";
     public static final String BEER_3_UPC = "0083783375213";
-
-    public static final UUID BEER_1_UUID = UUID.fromString("0a818933-087d-47f2-ad83-2f986ed087eb");
 
     private final BeerRepository beerRepository;
 
@@ -25,34 +23,34 @@ public class BeerLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        loadBeerObjects();
+        if(beerRepository.count() == 0) {
+            loadBeerObjects();
+        }
     }
 
     private void loadBeerObjects() {
-        if(beerRepository.count() == 0){
-            beerRepository.save(Beer.builder()
-                    .beerName("Dos XX")
-                    .beerStyle("Lager")
-                    .quantityToBrew(200)
-                    .minOnHand(12)
-                    .upc(BEER_1_UPC)
-                    .price(new BigDecimal("12.95")).build());
+        beerRepository.save(Beer.builder()
+                .beerName("Mango Bobs")
+                .beerStyle(BeerStyleEnum.IPA.name())
+                .quantityToBrew(200)
+                .minOnHand(12)
+                .upc(BEER_1_UPC)
+                .price(new BigDecimal("12.95")).build());
 
-            beerRepository.save(Beer.builder()
-                    .beerName("Corona")
-                    .beerStyle("Clara")
-                    .quantityToBrew(350)
-                    .minOnHand(12)
-                    .upc(BEER_2_UPC)
-                    .price(new BigDecimal("10.40")).build());
+        beerRepository.save(Beer.builder()
+                .beerName("Galaxy Cat")
+                .beerStyle(BeerStyleEnum.PALE_ALE.name())
+                .quantityToBrew(200)
+                .minOnHand(12)
+                .upc(BEER_2_UPC)
+                .price(new BigDecimal("12.95")).build());
 
-            beerRepository.save(Beer.builder()
-                    .beerName("Modelo")
-                    .beerStyle("Clara")
-                    .quantityToBrew(400)
-                    .minOnHand(12)
-                    .upc(BEER_3_UPC)
-                    .price(new BigDecimal("17.90")).build());
-        }
+        beerRepository.save(Beer.builder()
+                .beerName("Pinball Porter")
+                .beerStyle(BeerStyleEnum.PALE_ALE.name())
+                .quantityToBrew(200)
+                .minOnHand(12)
+                .upc(BEER_3_UPC)
+                .price(new BigDecimal("12.95")).build());
     }
 }
--- src/main/java/org/beer/works/beerservice/domain/Beer.java
+++ src/main/java/org/beer/works/beerservice/domain/Beer.java
@@ -4,10 +4,12 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
+import java.sql.Types;
 import java.util.UUID;
 
 @Getter
@@ -21,6 +23,7 @@ public class Beer {
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @JdbcTypeCode(Types.VARCHAR)
     @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID id;
 
