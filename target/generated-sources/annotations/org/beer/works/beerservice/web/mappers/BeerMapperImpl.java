c68638f52b8315c97f1038e97e750a518b6b96cd
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2024-08-12 10:12:55 -0600

    Configure JMS. Closes #29

--- target/generated-sources/annotations/org/beer/works/beerservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/org/beer/works/beerservice/web/mappers/BeerMapperImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-08-08T16:07:13-0600",
+    date = "2024-08-12T10:05:20-0600",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.3 (Amazon.com Inc.)"
 )
 @Component
